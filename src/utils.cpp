527fd3d6205d3404daa8ea90f0fabcadf79909f1
manu98de@hotmail.it <manu98de@hotmail.it>
2020-03-05 18:10:30 +0100

    Robe

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 #include <map>
+#include <gmp.h>
 #include <gmpxx.h>
 
-void CopPair(uint64_t i, bool even, mpz_t m, mpz_t n)
+void CopPair(uint64_t i, bool even, mpz_class m, mpz_class n)
 {
     if (even)
     {
-        mpz_set_ui(m, 2);
-        mpz_set_ui(n, 1);
+        m= 2;
+        n = 1;
     }
     else
     {
-        mpz_set_ui(m, 3);
-        mpz_set_ui(n, 1);
+        m = 3;
+        n = 1;
     }
     if (i == 0)
         return;
@@ -26,37 +27,31 @@ void CopPair(uint64_t i, bool even, mpz_t m, mpz_t n)
     }
     type.push_back((ushort)i);
 
-    mpz_t old_m, old_n;
-    mpz_init(old_m);
-    mpz_init(old_n);
+    mpz_class old_m=0;
 
     for (size_t j = type.size(); j > 0; j--)
     {
-        //TODO:ottimizzare
+        //TODO:ottimizzare scambi
+        //TODO:ottimizzare scambi
         if (type[j - 1] == 0)
         {
-            mpz_set(old_m, m);
-            mpz_mul_ui(m, m, 2);
-            mpz_sub(m, m, n);
-            mpz_set(n, old_m);
+            old_m = m;
+            m *= 2;
+            m -= n;
+            n = m;
         }
         else if (type[j - 1] == 1)
         {
-            mpz_set(old_m, m);
-            mpz_mul_ui(m, m, 2);
-            mpz_add(m, m, n);
-            mpz_set(n, old_m);
+            old_m = m;
+            m *= 2;
+            m += n;
+            n = m;
         }
         else
         {
-            mpz_set(old_n, n);
-            mpz_mul_ui(n, n, 2);
-            mpz_add(m, n, m);
-            mpz_set(n, old_n);
+            m += 2 * n;
         }
     }
-    mpz_clear(old_m);
-    mpz_clear(old_n);
 }
 
 void OddFirm(mpz_t n, mpz_t res)
@@ -101,7 +96,7 @@ void OddFirmScm(mpz_t n1, mpz_t n2, mpz_t n3, mpz_t n4, mpz_t res)
     mpz_clears(kn1, kn2, kn3, kn4, nullptr);
 }
 
-void FMK(mpz_t k, std::vector<mpz_t> &res, std::vector<uint64_t> &exp)
+void FMK(mpz_t k, std::vector<mpz_t*> &res, std::vector<uint64_t> &exp)
 {
     mpz_t tmp_k;
     mpz_init_set(tmp_k, k);
@@ -113,7 +108,9 @@ void FMK(mpz_t k, std::vector<mpz_t> &res, std::vector<uint64_t> &exp)
         if (mpz_divisible_p(tmp_k, i) != 0)
         {
             mpz_divexact(tmp_k, tmp_k, i);
-            res.push_back(i);
+            mpz_t f;
+            mpz_init_set(f, i);
+            res.push_back(&f);
             exp.push_back(1);
         }
     }
@@ -170,61 +167,45 @@ void MergeFactor(std::vector<mpz_t> &res, std::vector<uint64_t> &exp, size_t nIn
     }
 }
 
-void FactorizeMemoized(mpz_t key, mpz_t n, std::vector<mpz_t> &res, std::vector<uint64_t>& exp)
+void FactorizeMemoized(mpz_t key, mpz_t n, std::vector<mpz_t*> &res, std::vector<uint64_t>& exp)
 {
     FMK(key, res, exp); //fattorizza la chiave
     size_t nIndex = res.size(); //indice al quale inizieranno i fattori di n
-    FMN(n, res, exp); //fattorizza il coefficiente quadratico
+    //FMN(n, res, exp); //fattorizza il coefficiente quadratico
 
-    MergeFactor(res, exp, nIndex);
+    //MergeFactor(res, exp, nIndex);
 }
 
 void memoizedSearch(uint64_t min = 0, uint64_t max = 1000)
 {
-    struct mpzCmp
-    {
-        bool operator()(const mpz_t val1, const mpz_t val2) const
-        {
-            int res = mpz_cmp(val1, val2);
-            return res < 0;
-        }
-    };
-    std::map<mpz_t, mpz_t, mpzCmp> m;
+    std::map<mpz_class, mpz_class> m;
 
-    mpz_t a, b, apb, amb, n, oddKey;
-    mpz_inits(a, b, apb, amb, n, oddKey, nullptr);
+    mpz_class a=0, b=0, apb=0, amb=0, n=0, oddKey=0;
 
     for (uint64_t i = min; i < max; i++)
     {
         CopPair(i / 2, i % 2 == 0, a, b);
-        mpz_add(apb, a, b);
-        mpz_sub(amb, a, b);
-        OddFirmScm(a, b, apb, amb, oddKey);
+        apb = a + b;
+        amb = a - b;
+        //OddFirmScm(a, b, apb, amb, oddKey);
 
-        mpz_set_ui(n, 1);
-        mpz_mul(n, n, a);
-        mpz_mul(n, n, b);
-        mpz_mul(n, n, apb);
-        mpz_mul(n, n, amb);
+        n = a * b * apb * amb;
+        
         std::cout << "a: " << a << ";  b: " << b << std::endl;
-
         std::cout << "n: " << n << ";  oddKey: " << oddKey << std::endl;
 
-        if (mpz_divisible_p(n, oddKey) != 0)
+        if (mpz_divisible_p(n.get_mpz_t(), oddKey.get_mpz_t()) != 0)
         {
-            mpz_divexact(n, n, oddKey);
-            if (mpz_perfect_square_p(n))
+            mpz_divexact(n.get_mpz_t(), n.get_mpz_t(), oddKey.get_mpz_t());
+            if (mpz_perfect_square_p(n.get_mpz_t()))
             {
-                mpz_t oddKeyM, nM; //serve perchè oddKey è un puntatore, quindi cambiando il suo valore si cambierebbe anche il valore della chiave, stessa cosa per n
-                mpz_init_set(oddKeyM, oddKey);
-                mpz_init_set(nM, n);
-                if (m.find(oddKeyM) == m.end())
+                if (m.find(oddKey) == m.end())
                 {
-                    mpz_init_set(m[oddKeyM], nM);
+                    m[oddKey]= n;
                 }
                 else
                 {
-                    mpz_mul(m[oddKeyM], m[oddKeyM], nM);
+                    m[oddKey]*= n;
                 }
                 std::cout << "Giusto" << std::endl;
             }
@@ -239,6 +220,5 @@ void memoizedSearch(uint64_t min = 0, uint64_t max = 1000)
         }
     }
     std::cout << "m.size() is " << m.size() << std::endl;
-    mpz_clears(a, b, apb, amb, n, oddKey, nullptr);
     //TODO:bisognerebbe pulire le entry e chiavi della mappa
 }
